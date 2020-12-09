045fdf2ef8aaeb6b8721e0ad5b12b572a7616beb
juanan <juanan@mentalrepublic.com>
2020-12-09 11:20:48 +0100

    🦁 Day 9 Solution

--- README.md
+++ README.md
@@ -23,8 +23,8 @@ A good event to train any skill in any Language or just for problem resolution t
 | 05 | `Binary Boarding` | [Day 05](days/05) | `Javascript` | 🟢
 | 06 | `Custom Customs` | [Day 06](days/06) | `Javascript` | 🟢
 | 07 | `Handy Haversacks` | [Day 07](days/07) | `Javascript` | 🟢
-| 07 | `Handheld Halting` | [Day 08](days/08) | `Javascript` | 🟢
-| 09 |  | [Day 09](days/09) |  | 🔴
+| 08 | `Handheld Halting` | [Day 08](days/08) | `Javascript` | 🟢
+| 09 | `Encoding Error` | [Day 09](days/09) | `Javascript` | 🟢
 | 10 |  | [Day 10](days/10) |  | 🔴
 | 11 |  | [Day 11](days/11) |  | 🔴
 | 12 |  | [Day 12](days/12) |  | 🔴
---
+++ days/09/problem09.js
@@ -0,0 +1,46 @@
+// INPUT //
+const fs = require('fs');
+const string = fs.readFileSync(__dirname + '/input.txt').toString();
+
+// CONVERT TO ARRAY //
+const tray = string.split(/\n/g);
+
+// SEARCH THE INVALID SUM //
+function searchInvalidSumatory(array,pre) {
+    let sum = 0;
+    for (let i = pre; i < array.length; i++) {
+        let equal = true;
+        for (let y = i - pre; y < i; y++) {
+            for (let x = i - pre; x < i; x++) {
+                if(y != x && equal) sum = parseInt(array[x]) + parseInt(array[y]);
+                if(sum == parseInt(array[i])) equal = false;     
+            }
+        }
+        if(sum != array[i] && equal) return [parseInt(array[i]),i];
+    }
+}
+
+const error = searchInvalidSumatory(tray,25)
+
+// SEARCH CONTIGUOUS SUM //
+function searchContiguousSum(array,err) {
+    let testArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if(parseInt(array[i]) < err[0]) {
+            for (let x = i; x < array.length; x++) {
+                if(testArray.push(parseInt(array[x])) > 1 && testArray.reduce((a, b) => a + b, 0) == err[0]) return [Math.min(...testArray),Math.max(...testArray),Math.min(...testArray) + Math.max(...testArray)];
+            }
+            testArray = []
+        };
+    }
+}
+
+// LOGGING //
+console.time('Duration1');
+console.log('Invalid sum');
+console.log(searchInvalidSumatory(tray,25)[0]);
+console.timeEnd('Duration1');
+console.time('Duration2');
+console.log('Search the sums');
+console.log(searchContiguousSum(tray,error)[2]);
+console.timeEnd('Duration2');
